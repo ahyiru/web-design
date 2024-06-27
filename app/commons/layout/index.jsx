50a17fad317ad6a4e12951ba2ceea981d90e628f
ahyiru <ah.yiru@gmail.com>
2024-06-27 14:32:23 +0800

    fix: remove StrictMode

--- app/commons/layout/index.jsx
+++ app/commons/layout/index.jsx
@@ -1,13 +1,10 @@
-import {StrictMode} from 'react';
 import UiI18n from './components/uiI18n';
 import Layout from './layout';
 
 const Index = props => (
-  <StrictMode>
-    <UiI18n>
-      <Layout {...props} />
-    </UiI18n>
-  </StrictMode>
+  <UiI18n>
+    <Layout {...props} />
+  </UiI18n>
 );
 
 export default Index;
