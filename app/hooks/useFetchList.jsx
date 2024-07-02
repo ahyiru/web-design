5e5ae1313d75bb5e5d25448d4f757dcb7fd43432
ahyiru <ah.yiru@gmail.com>
2024-07-02 11:24:14 +0800

    fix: fix fetchList

--- app/hooks/useFetchList.jsx
+++ app/hooks/useFetchList.jsx
@@ -1,11 +1,15 @@
-import {useCallback, useEffect} from 'react';
+import {useCallback, useEffect, useRef} from 'react';
 import {useAsync} from '@huxy/use';
 
 const useFetchList = (fetchList, initParams = null, handleResult, commonParams = null) => {
   const [result, updateResult] = useAsync({});
+  const isMounted = useRef(false);
   const update = useCallback(params => fetchList && updateResult({res: fetchList({...commonParams, ...params})}, handleResult), []);
   useEffect(() => {
-    update({...initParams});
+    if (!isMounted.current) {
+      isMounted.current = true;
+      update({...initParams});
+    }
   }, []);
   const {res} = result;
 
