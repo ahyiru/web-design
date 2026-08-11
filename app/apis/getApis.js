850bb968471080e01cb8ce0adb7b7371786bb313
ahyiru <ah.yiru@gmail.com>
2026-08-11 12:01:32 +0800

    fix: fix apis

--- app/apis/getApis.js
+++ app/apis/getApis.js
@@ -3,6 +3,11 @@ import {defProject} from '@app/configs';
 
 const getApis = () => fetcher({url: '/apis/list', params: {projectId: defProject?.id, current: 1, size: 300}});
 
-const apis = (await getApis()).result?.list ?? [];
+let apis = [];
+try {
+  apis = (await getApis()).result?.list ?? [];
+} catch (err) {
+  console.error(err.message);
+}
 
 export default apis;
