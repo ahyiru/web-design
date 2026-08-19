28021f19f21cc6b3854cb77fbcebe54609adbe5f
ahyiru <ah.yiru@gmail.com>
2026-08-19 12:59:54 +0800

    fix: fix bugs

--- app/views/user/configs.js
+++ app/views/user/configs.js
@@ -4,12 +4,12 @@ import * as rules from '@app/utils/rules';
 export const formRules = rules;
 
 export const githubConfigs = {
-  github_client_id: '61721ef923095e006d18',
+  github_client_id: 'xxx',
   github_oauth_url: 'https://github.com/login/oauth/authorize',
 };
 
 export const wechatConfigs = {
-  appid: 'wx7f4df123f88372a5',
+  appid: 'xxx',
   wechat_oauth_url: 'https://open.weixin.qq.com/connect/oauth2/authorize',
   redirect_uri: 'https://ihuxy.com/user/signin',
   response_type: 'code',
